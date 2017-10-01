5f420844391af1362ddcc071dd4500b6002123b8
ahmadlakiayat <you@example.com>
2017-10-01 21:18:06 +0300

    all tables created

---
+++ database/migrations/2017_10_01_180841_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('body');
+            $table->integer('create_notification');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
